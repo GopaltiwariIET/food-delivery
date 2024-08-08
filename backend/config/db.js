9c1d72c9883ca1401ab7951ebed0c6a378f1818c
GopaltiwariIET <gopaltiwari28122003@gmail.com>
2024-08-09 02:29:08 +0530

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(process.env.MONGO_DB_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     })
