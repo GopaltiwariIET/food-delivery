a842b35dbf8aeb85662383e29215cfaf8d8c6163
GopaltiwariIET <gopaltiwari28122003@gmail.com>
2024-08-13 04:38:06 +0530

    update url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,8 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  // const url = "https://food-delivery-zv48.onrender.com";
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-zv48.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
