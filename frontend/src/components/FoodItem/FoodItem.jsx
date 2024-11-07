425dab2e018c89f368a19a41cc51127df62a4276
GopaltiwariIET <gopaltiwari28122003@gmail.com>
2024-11-07 21:17:04 +0530

    fix image

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -5,46 +5,46 @@ import { useState } from "react";
 import { StoreContext } from "../../context/StoreContext";
 
 function FoodItem({ id, name, price, description, image }) {
-  const { cartItems, addToCart, removeFromCart, url } =
-    useContext(StoreContext);
+	const { cartItems, addToCart, removeFromCart, url } =
+		useContext(StoreContext);
 
-  return (
-    <div className="food-item">
-      <div className="food-item-img-container">
-        <img className="food-item-image" src={image} alt="" />
-        {!cartItems[id] ? (
-          <img
-            className="add"
-            onClick={() => addToCart(id)}
-            src={assets.add_icon_white}
-            alt=""
-          />
-        ) : (
-          <div className="food-item-counter">
-            <img
-              onClick={() => removeFromCart(id)}
-              src={assets.remove_icon_red}
-              alt=""
-            />
-            <p className="cartitemsp">{cartItems[id]}</p>
-            <img
-              onClick={() => addToCart(id)}
-              src={assets.add_icon_green}
-              alt=""
-            />
-          </div>
-        )}
-      </div>
-      <div className="food-item-info">
-        <div className="food-item-name-rating">
-          <p className="namewe">{name}</p>
-          <img className="ratingstars" src={assets.rating_starts} alt="" />
-        </div>
-        <p className="food-item-desc">{description}</p>
-        <p className="food-item-price">${price}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="food-item">
+			<div className="food-item-img-container">
+				<img className="food-item-image" height={180} src={image} alt="" />
+				{!cartItems[id] ? (
+					<img
+						className="add"
+						onClick={() => addToCart(id)}
+						src={assets.add_icon_white}
+						alt=""
+					/>
+				) : (
+					<div className="food-item-counter">
+						<img
+							onClick={() => removeFromCart(id)}
+							src={assets.remove_icon_red}
+							alt=""
+						/>
+						<p className="cartitemsp">{cartItems[id]}</p>
+						<img
+							onClick={() => addToCart(id)}
+							src={assets.add_icon_green}
+							alt=""
+						/>
+					</div>
+				)}
+			</div>
+			<div className="food-item-info">
+				<div className="food-item-name-rating">
+					<p className="namewe">{name}</p>
+					<img className="ratingstars" src={assets.rating_starts} alt="" />
+				</div>
+				<p className="food-item-desc">{description}</p>
+				<p className="food-item-price">${price}</p>
+			</div>
+		</div>
+	);
 }
 
 export default FoodItem;
